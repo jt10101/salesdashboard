0c30156bfe7aa386ddaa94174f0729078d89d2cd
Jacob <jacobtham3@gmail.com>
2025-07-17 08:09:03 +0800

    backend route for index transactions

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -16,4 +16,19 @@ const addTransaction = async (req, res) => {
   }
 };
 
-module.exports = { addTransaction };
+const indexTransaction = async (req, res) => {
+  try {
+    const userId = req.user?._id;
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized: User not found" });
+    }
+
+    const data = await Transaction.find({ salesPersonId: userId });
+
+    res.status(200).json({ data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { addTransaction, indexTransaction };
--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { addTransaction } = require("../controllers/transactionController");
+const {
+  addTransaction,
+  indexTransaction,
+} = require("../controllers/transactionController");
 
 //user routes
 router.post("/add", addTransaction);
+router.get("/index", indexTransaction);
 
 module.exports = router;
